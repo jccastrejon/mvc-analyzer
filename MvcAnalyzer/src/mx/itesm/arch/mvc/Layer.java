69b63e98122a457c8028fe3aa08885435ec89160
jccastrejon <jccastrejon@fafc8db4-c2b9-11de-a0e9-83d03c287164>
2009-11-10 19:42:39 +0000

    Added support for graphic export of the MVC layers classification

---
+++ MvcAnalyzer/src/mx/itesm/arch/mvc/Layer.java
@@ -0,0 +1,49 @@
+package mx.itesm.arch.mvc;
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+
+/**
+ * @author jccastrejon
+ * 
+ */
+public enum Layer {
+    Model("#FFFF5C"), View("#99CCFF"), Controller("#FF9999");
+
+    /**
+     * 
+     */
+    private String rgbColor;
+
+    /**
+     * Variable attribute.
+     */
+    public static Attribute attribute;
+
+    static {
+	FastVector valuesVector;
+
+	valuesVector = new FastVector(Layer.values().length);
+	for (Layer layer : Layer.values()) {
+	    valuesVector.addElement(layer.toString());
+	}
+
+	attribute = new Attribute("Layer", valuesVector, MvcAnalyzer.Variable.values().length + 1);
+    }
+
+    /**
+     * 
+     * @param rgbColor
+     */
+    private Layer(final String rgbColor) {
+	this.rgbColor = rgbColor;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getRgbColor() {
+	return this.rgbColor;
+    }
+}
--- MvcAnalyzer/tst/mx/itesm/arch/mvc/MvcAnalyzerTest.java
+++ MvcAnalyzer/tst/mx/itesm/arch/mvc/MvcAnalyzerTest.java
@@ -1,5 +1,6 @@
 package mx.itesm.arch.mvc;
 
+import java.io.File;
 import java.io.IOException;
 
 import junit.framework.TestCase;
@@ -15,7 +16,13 @@ public class MvcAnalyzerTest extends TestCase {
      * 
      */
     public void testClassifyClassesInDirectory() throws Exception {
-	MvcAnalyzer.classifyClassesInDirectory("bin");
+	File analyzedFile;
+	File outputFile;
+
+	analyzedFile = new File("bin");
+	outputFile = new File("img/MvcAnalyzerClassification.svg");
+	outputFile.delete();
+	MvcAnalyzer.classifyClassesInDirectory(analyzedFile, outputFile);
     }
 
     /**
@@ -23,7 +30,13 @@ public class MvcAnalyzerTest extends TestCase {
      * @throws Exception
      */
     public void testClassifyClassesInWar() throws Exception {
-	MvcAnalyzer
-		.classifyClassesinWar("/home/jccastrejon/java/spring-framework-2.5.6.SEC01/samples/petclinic/dist/petclinic.war");
+	File analyzedFile;
+	File outputFile;
+
+	analyzedFile = new File(
+		"/home/jccastrejon/java/spring-framework-2.5.6.SEC01/samples/petclinic/dist/petclinic.war");
+	outputFile = new File("img/petclinicClassification.svg");
+	outputFile.delete();
+	MvcAnalyzer.classifyClassesinWar(analyzedFile, outputFile);
     }
 }
