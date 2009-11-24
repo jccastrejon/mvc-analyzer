3712b156022b0a9756dbe1207c2b6217f3707090
jccastrejon <jccastrejon@fafc8db4-c2b9-11de-a0e9-83d03c287164>
2009-11-24 00:17:23 +0000

    Added After classifying the poroject's components, look for invalid relations between MVC layers

--- trunk/MvcAnalyzer/src/mx/itesm/arch/mvc/Layer.java
+++ trunk/MvcAnalyzer/src/mx/itesm/arch/mvc/Layer.java
@@ -10,10 +10,54 @@ import weka.core.FastVector;
  * 
  */
 public enum Layer {
-    Model("#FFFF5C"), View("#99CCFF"), Controller("#FF9999");
+    Model("#B1FF3D", "filled") {
+	@Override
+	public boolean isValidRelation(final Layer layer) {
+	    boolean returnValue;
+
+	    // The Model layer should only depend on itself
+	    returnValue = true;
+	    if (layer != Model) {
+		returnValue = false;
+	    }
+
+	    return returnValue;
+	}
+    },
+    View("#3399FF", "filled") {
+	@Override
+	public boolean isValidRelation(final Layer layer) {
+	    boolean returnValue;
+
+	    // The View layer shouldn't depend directly on the Model, the
+	    // Controller should provide the required beans, of if the model
+	    // classes are used directly on the view, the class names shouldn't
+	    // be directly exposed
+	    returnValue = true;
+	    if (layer == Model) {
+		returnValue = false;
+	    }
+
+	    return returnValue;
+	}
+    },
+    Controller("#A3A3A3", "filled") {
+	@Override
+	public boolean isValidRelation(final Layer layer) {
+	    // The Controller layer is the intermediary between all layers
+	    return true;
+	}
+    },
+    InvalidModel("#B1FF3D", "rounded"), InvalidView("#3399FF", "rounded"), InvalidController(
+	    "#A3A3A3", "rounded");
 
     /**
-     * RGB color associated to the layer.
+     * Layer style used in the graphic export.
+     */
+    private String style;
+
+    /**
+     * RGB color associated to the layer, used in the graphic export.
      */
     private String rgbColor;
 
@@ -25,6 +69,7 @@ public enum Layer {
     static {
 	FastVector valuesVector;
 
+	// Initialize the attribute's data
 	valuesVector = new FastVector(Layer.values().length);
 	for (Layer layer : Layer.values()) {
 	    valuesVector.addElement(layer.toString());
@@ -34,18 +79,46 @@ public enum Layer {
     }
 
     /**
+     * Check if a relation with the given layer is valid or not in a MVC design.
      * 
-     * @param rgbColor
+     * @param layer
+     *            Layer to compare with.
+     * @return <em>true</em> if the relation is valid, <em>false</em> otherwise.
      */
-    private Layer(final String rgbColor) {
-	this.rgbColor = rgbColor;
+    public boolean isValidRelation(final Layer layer) {
+	return true;
     }
 
     /**
+     * Full constructor that specifies the color to be used in the graphic
+     * export.
      * 
-     * @return
+     * @param rgbColor
+     *            RGB color.
+     * @param style
+     *            Layer style.
+     * 
+     */
+    private Layer(final String rgbColor, final String style) {
+	this.rgbColor = rgbColor;
+	this.style = style;
+    }
+
+    /**
+     * Get the Layer's RGB color.
+     * 
+     * @return RGB color.
      */
     public String getRgbColor() {
 	return this.rgbColor;
+    }
+
+    /**
+     * Get the Layer's style.
+     * 
+     * @return Layer's style.
+     */
+    public String getStyle() {
+	return this.style;
     }
 }
